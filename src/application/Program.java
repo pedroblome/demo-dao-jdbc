b4216aa92a5f9d32c624d275fc332a7c6be08c45
pedroblome <joaopedroblome78@gmail.com>
2021-09-12 19:58:12 -0300

    reusing instantiation

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,9 +12,9 @@ public class Program {
 	public static void main(String[] args) {
 
 		SellerDao SellerDao = DaoFactory.createSellerDao();
-
+		
+		System.out.println("===TEST 1: seller findById =======");
 		Seller seller = SellerDao.findById(3);
-
 		System.out.println(seller);
 
 	}
