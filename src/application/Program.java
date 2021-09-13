0df1e39bca4d80c1b88bd414d8746795824ef9d5
pedroblome <joaopedroblome78@gmail.com>
2021-09-13 11:56:14 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -30,6 +30,14 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-
+		System.out.println("\n===TEST 4: seller insert =======");
+		Seller newSeller = new Seller(null, "greg","greg@gmail.com", new Date(), 4000.0, department);
+		SellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = "+newSeller.getId());
+		
+		System.out.println("\n===TEST 5: Owner insert =======");
+		Seller newOwner = new Seller(null, "sid","sidnando@gmail.com", new Date(), 120000.0, department);
+		SellerDao.insert(newOwner);
+		System.out.println("Inserted! New id = "+newOwner.getId());
 	}
 }
